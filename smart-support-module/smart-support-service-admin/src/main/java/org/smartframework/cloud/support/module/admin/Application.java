4450618ddeda93daebaa95f557d92490ad5645dc
luckyQing <1634753825@qq.com>
2019-07-09 01:07:27 +0800

    修改basic服务名：basic=>support

---
+++ smart-support-module/smart-support-service-admin/src/main/java/org/smartframework/cloud/support/module/admin/Application.java
@@ -0,0 +1,18 @@
+package org.smartframework.cloud.support.module.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
