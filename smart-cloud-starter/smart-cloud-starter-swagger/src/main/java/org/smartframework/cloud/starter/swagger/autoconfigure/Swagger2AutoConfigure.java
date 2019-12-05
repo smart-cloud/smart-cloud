03f18adc20cd58e682ea8b1388de0d2951691562
luckyQing <1634753825@qq.com>
2019-12-05 21:15:59 +0800

    refactor: remove swagger2markdown

--- smart-cloud-starter/smart-cloud-starter-swagger/src/main/java/org/smartframework/cloud/starter/swagger/autoconfigure/Swagger2AutoConfigure.java
+++ smart-cloud-starter/smart-cloud-starter-swagger/src/main/java/org/smartframework/cloud/starter/swagger/autoconfigure/Swagger2AutoConfigure.java
@@ -8,14 +8,9 @@ import java.util.List;
 import org.smartframework.cloud.starter.common.constants.PackageConfig;
 import org.smartframework.cloud.starter.configure.properties.SmartProperties;
 import org.smartframework.cloud.starter.configure.properties.SwaggerProperties;
-import org.smartframework.cloud.starter.swagger.condition.UploadSwaggerCondition;
-import org.smartframework.cloud.starter.swagger.core.Swagger2Markdown;
-import org.smartframework.cloud.starter.swagger.listener.UploadSwaggerListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -24,10 +19,13 @@ import com.google.common.base.Predicate;
 
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.ApiSelector;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -50,16 +48,62 @@ public class Swagger2AutoConfigure {
 
 	@Bean
 	public Docket docket() {
+		// ApiSelector
 		List<Predicate<RequestHandler>> requestHandlers = new ArrayList<>();
 		String[] basePackages = PackageConfig.getBasePackages();
 		for (String basePackage : basePackages) {
 			requestHandlers.add(RequestHandlerSelectors.basePackage(basePackage));
 		}
 		ApiSelector apiSelector = new ApiSelector(or(requestHandlers), PathSelectors.any());
-		return new Docket(DocumentationType.SWAGGER_2).groupName(smartProperties.getSwagger().getGroupName())
-				.genericModelSubstitutes(DeferredResult.class).useDefaultResponseMessages(false)
-				.forCodeGeneration(false).apiInfo(apiInfo()).select()
-				.apis(apiSelector.getRequestHandlerSelector()).paths(PathSelectors.any()).build();
+		
+		return new Docket(DocumentationType.SPRING_WEB)
+				.groupName(smartProperties.getSwagger().getGroupName())
+				.genericModelSubstitutes(DeferredResult.class)
+				.useDefaultResponseMessages(false)
+				.forCodeGeneration(false)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(apiSelector.getRequestHandlerSelector())
+				.paths(PathSelectors.any())
+				.build()
+				.globalOperationParameters(buildParameters());
+	}
+	
+	private List<Parameter> buildParameters(){
+		List<Parameter> parameters = new ArrayList<>();
+		parameters.add(new ParameterBuilder()
+				.parameterType("header")
+				.required(true)
+				.name("smart-sign")
+				.description("签名")
+				.modelRef(new ModelRef("string"))
+				.scalarExample("109ad1a8e05f8de345e6d780f09b001e97dc3d6fa9bbbe6936edb2b75a81864ac3b0b071e093af001fbffa479217540138b98f6f165e8246dd25a2536649f1f6")
+				.build());
+		parameters.add(new ParameterBuilder()
+				.parameterType("header")
+				.required(true)
+				.name("smart-timestamp")
+				.description("请求时间戳（单位秒）")
+				.modelRef(new ModelRef("long"))
+				.scalarExample(1555778393862L)
+				.build());
+		parameters.add(new ParameterBuilder()
+				.parameterType("header")
+				.required(true)
+				.name("smart-token")
+				.description("请求token")
+				.modelRef(new ModelRef("string"))
+				.scalarExample("4c2e22605001000rK")
+				.build());
+		parameters.add(new ParameterBuilder()
+				.parameterType("header")
+				.required(true)
+				.name("smart-nonce")
+				.description("请求流水号")
+				.modelRef(new ModelRef("string"))
+				.scalarExample("eb9f81e7cee1c000")
+				.build());
+        return parameters;
 	}
 
 	private ApiInfo apiInfo() {
@@ -71,23 +115,6 @@ public class Swagger2AutoConfigure {
 				.contact(contact)
 				.version(smartProperties.getApi().getApiVersion())
 				.build();
-	}
-
-	@Configuration
-	@Conditional(UploadSwaggerCondition.class)
-	static class UploadSwaggerAutoConfigure {
-		
-		@Bean
-		public Swagger2Markdown swagger2Markdown(@Value("${server.port}") String port,
-				final SmartProperties smartProperties) {
-			return new Swagger2Markdown(port, smartProperties.getSwagger().getGroupName());
-		}
-
-		@Bean
-		public UploadSwaggerListener uploadSwaggerListener(final Swagger2Markdown swagger2Markdown) {
-			return new UploadSwaggerListener(swagger2Markdown);
-		}
-		
 	}
 
 }
