bd22d082503ef289548178ae1e9d35420024f2f9
luckyQing <1634753825@qq.com>
2020-07-13 19:26:09 +0800

    fixbug: fix class not found

---
+++ smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/autoconfigure/ReactiveFilterAutoConfigure.java
@@ -0,0 +1,17 @@
+package org.smartframework.cloud.starter.core.business.autoconfigure;
+
+import org.smartframework.cloud.starter.core.business.security.filter.ReactiveFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+public class ReactiveFilterAutoConfigure {
+
+	@Bean
+	public ReactiveFilter reactiveFilter() {
+		return new ReactiveFilter();
+	}
+
+}
--- smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/autoconfigure/ServletFilterAutoConfigure.java
+++ smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/autoconfigure/ServletFilterAutoConfigure.java
@@ -1,19 +1,20 @@
 package org.smartframework.cloud.starter.core.business.autoconfigure;
 
-import javax.servlet.Filter;
-
-import org.smartframework.cloud.starter.core.business.security.filter.ReactiveFilter;
 import org.smartframework.cloud.starter.core.business.security.filter.ServletFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
+
 @Configuration
-public class SmartFilterAutoConfigure {
+@ConditionalOnClass(name={"javax.servlet.Filter"})
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class ServletFilterAutoConfigure {
 
 	@Bean
-	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 	public FilterRegistrationBean<Filter> registFilter() {
 		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
 		registration.setFilter(new ServletFilter());
@@ -23,10 +24,4 @@ public class SmartFilterAutoConfigure {
 		return registration;
 	}
 	
-	@Bean
-	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
-	public ReactiveFilter reactiveFilter() {
-		return new ReactiveFilter();
-	}
-
 }
