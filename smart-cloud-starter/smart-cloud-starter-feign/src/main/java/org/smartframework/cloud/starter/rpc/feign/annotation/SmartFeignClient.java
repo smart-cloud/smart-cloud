5ab81f34e05d1b1d895573ef61f913fcb9b9d96e
luckyQing <1634753825@qq.com>
2020-07-13 19:36:27 +0800

    fixbug: fix feign configration bean name repeat error

--- smart-cloud-starter/smart-cloud-starter-feign/src/main/java/org/smartframework/cloud/starter/rpc/feign/annotation/SmartFeignClient.java
+++ smart-cloud-starter/smart-cloud-starter-feign/src/main/java/org/smartframework/cloud/starter/rpc/feign/annotation/SmartFeignClient.java
@@ -1,18 +1,12 @@
 package org.smartframework.cloud.starter.rpc.feign.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.smartframework.cloud.starter.rpc.feign.condition.SmartFeignClientCondition;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.annotation.AliasFor;
-
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.lang.annotation.*;
 
 /**
  * <code>FeignClient</code>自定义条件封装
@@ -34,6 +28,9 @@ public @interface SmartFeignClient {
 
 	@AliasFor(annotation = FeignClient.class)
 	String url() default "";
+
+	@AliasFor(annotation = FeignClient.class)
+	String contextId() default "";
 
 	@AliasFor(annotation = FeignClient.class)
 	Class<?> fallback() default void.class;
