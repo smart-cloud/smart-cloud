04bd42a0702fd115054488b3d33c936d8b25ae8a
luckyQing <1634753825@qq.com>
2020-09-08 13:58:21 +0800

    fixbug: fix NPE

--- smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/util/WebReactiveUtil.java
+++ smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/util/WebReactiveUtil.java
@@ -1,8 +1,6 @@
 package org.smartframework.cloud.starter.core.business.util;
 
-import java.io.Serializable;
-import java.util.stream.Stream;
-
+import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.smartframework.cloud.common.pojo.enums.ReturnCodeEnum;
@@ -12,7 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 
-import lombok.experimental.UtilityClass;
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.util.stream.Stream;
 
 /**
  * web相关工具类
@@ -22,126 +22,129 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class WebReactiveUtil {
-	
-	/**
-	 * 获取请求的ip地址
-	 *
-	 * @return ip地址
-	 */
-	public static String getRealIP() {
-		HttpHeaders httpHeaders = ReactiveRequestContextHolder.getHttpHeaders();
-		if (httpHeaders == null) {
-			return null;
-		}
-		String ip = httpHeaders.getFirst("x-forwarded-for");
-		String unknown = "unknown";
-		if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
-			ip = httpHeaders.getFirst("Proxy-Client-IP");
-		}
-		if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
-			ip = httpHeaders.getFirst("WL-Proxy-Client-IP");
-		}
-		if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
-			ip = ReactiveRequestContextHolder.getServerHttpRequest().getRemoteAddress().toString();
-		}
-		if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
-			ip = httpHeaders.getFirst("http_client_ip");
-		}
-		if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
-			ip = httpHeaders.getFirst("HTTP_X_FORWARDED_FOR");
-		}
 
-		// 如果是多级代理，那么取第一个ip为客户ip
-		if (ip != null && ip.contains(",")) {
-			ip = ip.substring(ip.lastIndexOf(',') + 1, ip.length()).trim();
-		}
-		return ip;
-	}
+    /**
+     * 获取请求的ip地址
+     *
+     * @return ip地址
+     */
+    public static String getRealIP() {
+        HttpHeaders httpHeaders = ReactiveRequestContextHolder.getHttpHeaders();
+        if (httpHeaders == null) {
+            return null;
+        }
+        String ip = httpHeaders.getFirst("x-forwarded-for");
+        String unknown = "unknown";
+        if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = httpHeaders.getFirst("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = httpHeaders.getFirst("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
+            InetSocketAddress inetSocketAddress = ReactiveRequestContextHolder.getServerHttpRequest().getRemoteAddress();
+            if (inetSocketAddress != null) {
+                ip = inetSocketAddress.toString();
+            }
+        }
+        if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = httpHeaders.getFirst("http_client_ip");
+        }
+        if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = httpHeaders.getFirst("HTTP_X_FORWARDED_FOR");
+        }
 
-	/**
-	 * 获取有效的请求参数（过滤掉不能序列化的）
-	 * 
-	 * @param args
-	 * @return
-	 */
-	public static Object getRequestArgs(Object[] args) {
-		if (ArrayUtils.isEmpty(args)) {
-			return args;
-		}
+        // 如果是多级代理，那么取第一个ip为客户ip
+        if (ip != null && ip.contains(",")) {
+            ip = ip.substring(ip.lastIndexOf(',') + 1, ip.length()).trim();
+        }
+        return ip;
+    }
 
-		boolean needFilter = false;
-		for (Object arg : args) {
-			if (needFilter(arg)) {
-				needFilter = true;
-				break;
-			}
-		}
+    /**
+     * 获取有效的请求参数（过滤掉不能序列化的）
+     *
+     * @param args
+     * @return
+     */
+    public static Object getRequestArgs(Object[] args) {
+        if (ArrayUtils.isEmpty(args)) {
+            return args;
+        }
 
-		if (!needFilter) {
-			return args.length == 1 ? args[0] : args;
-		}
+        boolean needFilter = false;
+        for (Object arg : args) {
+            if (needFilter(arg)) {
+                needFilter = true;
+                break;
+            }
+        }
 
-		Object[] tempArgs = Stream.of(args).filter(arg -> !needFilter(arg)).toArray();
+        if (!needFilter) {
+            return args.length == 1 ? args[0] : args;
+        }
 
-		return getValidArgs(tempArgs);
-	}
+        Object[] tempArgs = Stream.of(args).filter(arg -> !needFilter(arg)).toArray();
 
-	/**
-	 * 是否需要过滤
-	 * 
-	 * @param object
-	 * @return
-	 */
-	private static boolean needFilter(Object object) {
-		return !(object instanceof Serializable);
-	}
+        return getValidArgs(tempArgs);
+    }
 
-	/**
-	 * 获取有效的参数（如果是request对象，则优先从MultiValueMap里取）
-	 * 
-	 * @param args
-	 * @return
-	 */
-	private static Object getValidArgs(Object[] args) {
-		if (ArrayUtils.isEmpty(args)) {
-			return args;
-		}
+    /**
+     * 是否需要过滤
+     *
+     * @param object
+     * @return
+     */
+    private static boolean needFilter(Object object) {
+        return !(object instanceof Serializable);
+    }
 
-		if (args.length == 1 && args[0] instanceof ServerHttpRequest) {
-			ServerHttpRequest request = (ServerHttpRequest) args[0];
-			return request.getQueryParams();
-		}
+    /**
+     * 获取有效的参数（如果是request对象，则优先从MultiValueMap里取）
+     *
+     * @param args
+     * @return
+     */
+    private static Object getValidArgs(Object[] args) {
+        if (ArrayUtils.isEmpty(args)) {
+            return args;
+        }
 
-		return args;
-	}
+        if (args.length == 1 && args[0] instanceof ServerHttpRequest) {
+            ServerHttpRequest request = (ServerHttpRequest) args[0];
+            return request.getQueryParams();
+        }
 
-	/**
-	 * 获取HttpServletRequest
-	 * 
-	 * @return
-	 */
-	public static ServerHttpRequest getServerHttpRequest() {
-		ServerHttpRequest request = ReactiveRequestContextHolder.getServerHttpRequest();
-		if(request!=null) {
-			return request;
-		}
-		
-		throw new ServerException(ReturnCodeEnum.GET_REQUEST_FAIL);
-	}
-	
+        return args;
+    }
 
-	/**
-	 * 获取HttpServletResponse
-	 * 
-	 * @return
-	 */
-	public static ServerHttpResponse getServerHttpResponse() {
-		ServerHttpResponse response = ReactiveRequestContextHolder.getServerHttpResponse();
-		if(response!=null) {
-			return response;
-		}
-		
-		throw new ServerException(ReturnCodeEnum.GET_RESPONSE_FAIL);
-	}
-	
+    /**
+     * 获取HttpServletRequest
+     *
+     * @return
+     */
+    public static ServerHttpRequest getServerHttpRequest() {
+        ServerHttpRequest request = ReactiveRequestContextHolder.getServerHttpRequest();
+        if (request != null) {
+            return request;
+        }
+
+        throw new ServerException(ReturnCodeEnum.GET_REQUEST_FAIL);
+    }
+
+
+    /**
+     * 获取HttpServletResponse
+     *
+     * @return
+     */
+    public static ServerHttpResponse getServerHttpResponse() {
+        ServerHttpResponse response = ReactiveRequestContextHolder.getServerHttpResponse();
+        if (response != null) {
+            return response;
+        }
+
+        throw new ServerException(ReturnCodeEnum.GET_RESPONSE_FAIL);
+    }
+
 }
