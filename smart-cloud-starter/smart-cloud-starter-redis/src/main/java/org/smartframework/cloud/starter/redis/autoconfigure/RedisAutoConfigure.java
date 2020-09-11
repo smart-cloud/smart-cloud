1a6f035beb99ae2250964982f12cdf5f0ebc1d34
luckyQing <1634753825@qq.com>
2020-09-11 18:38:06 +0800

    fixbug: redis serialization garbled

--- smart-cloud-starter/smart-cloud-starter-redis/src/main/java/org/smartframework/cloud/starter/redis/autoconfigure/RedisAutoConfigure.java
+++ smart-cloud-starter/smart-cloud-starter-redis/src/main/java/org/smartframework/cloud/starter/redis/autoconfigure/RedisAutoConfigure.java
@@ -1,7 +1,5 @@
 package org.smartframework.cloud.starter.redis.autoconfigure;
 
-import java.io.Serializable;
-
 import org.redisson.Redisson;
 import org.smartframework.cloud.starter.redis.component.RedisComponent;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -10,16 +8,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 /**
  * redis配置
@@ -28,70 +23,71 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
  * @date 2018-10-17
  */
 @Configuration
-@ConditionalOnClass({ Redisson.class, RedisOperations.class })
+@ConditionalOnClass({Redisson.class, RedisOperations.class})
 @EnableCaching
 public class RedisAutoConfigure {
 
-	@Bean
-	public RedisTemplate<Serializable, Serializable> initRedisTemplate(final RedisConnectionFactory connectionFactory) {
-		// 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
-		RedisTemplate<Serializable, Serializable> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(connectionFactory);
-		redisTemplate.setEnableDefaultSerializer(true);
-		redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-		redisTemplate.afterPropertiesSet();
-		return redisTemplate;
-	}
+    @Primary
+    @Bean
+    public RedisTemplate<Serializable, Serializable> initRedisTemplate(final RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Serializable, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
 
-	@Bean
-	public StringRedisTemplate initStringRedisTemplate(final RedisConnectionFactory connectionFactory) {
-		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-		stringRedisTemplate.setConnectionFactory(connectionFactory);
-		stringRedisTemplate.setEnableDefaultSerializer(true);
-		stringRedisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+    @Bean
+    public StringRedisTemplate initStringRedisTemplate(final RedisConnectionFactory connectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(connectionFactory);
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        stringRedisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return stringRedisTemplate;
+    }
 
-		stringRedisTemplate.afterPropertiesSet();
-		return stringRedisTemplate;
-	}
+    @Bean
+    public CacheManager cacheManager() {
+        return new SimpleCacheManager();
+    }
 
-	@Bean
-	public CacheManager cacheManager() {
-		return new SimpleCacheManager();
-	}
+    @Bean
+    public ValueOperations<Serializable, Serializable> redisValueOperations(
+            final RedisTemplate<Serializable, Serializable> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
 
-	@Bean
-	public ValueOperations<Serializable, Serializable> redisValueOperations(
-			final RedisTemplate<Serializable, Serializable> redisTemplate) {
-		return redisTemplate.opsForValue();
-	}
+    @Bean
+    public ListOperations<Serializable, Serializable> redisListOperations(
+            final RedisTemplate<Serializable, Serializable> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
 
-	@Bean
-	public ListOperations<Serializable, Serializable> redisListOperations(
-			final RedisTemplate<Serializable, Serializable> redisTemplate) {
-		return redisTemplate.opsForList();
-	}
+    @Bean
+    public SetOperations<Serializable, Serializable> redisSetOperations(
+            final RedisTemplate<Serializable, Serializable> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
 
-	@Bean
-	public SetOperations<Serializable, Serializable> redisSetOperations(
-			final RedisTemplate<Serializable, Serializable> redisTemplate) {
-		return redisTemplate.opsForSet();
-	}
+    @Bean
+    public ZSetOperations<Serializable, Serializable> redisZSetOperations(
+            final RedisTemplate<Serializable, Serializable> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
 
-	@Bean
-	public ZSetOperations<Serializable, Serializable> redisZSetOperations(
-			final RedisTemplate<Serializable, Serializable> redisTemplate) {
-		return redisTemplate.opsForZSet();
-	}
+    @Bean
+    public HashOperations<Serializable, Serializable, Serializable> redisHashOperations(
+            final RedisTemplate<Serializable, Serializable> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
 
-	@Bean
-	public HashOperations<Serializable, Serializable, Serializable> redisHashOperations(
-			final RedisTemplate<Serializable, Serializable> redisTemplate) {
-		return redisTemplate.opsForHash();
-	}
-
-	@Bean
-	public RedisComponent redisComponent(final StringRedisTemplate stringRedisTemplate) {
-		return new RedisComponent(stringRedisTemplate);
-	}
+    @Bean
+    public RedisComponent redisComponent(final StringRedisTemplate stringRedisTemplate) {
+        return new RedisComponent(stringRedisTemplate);
+    }
 
 }
