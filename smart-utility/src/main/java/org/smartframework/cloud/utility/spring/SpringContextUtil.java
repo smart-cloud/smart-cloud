7bcad043ba1cf0dc5d2e26ffc5a9e6b81155d744
luckyQing <1634753825@qq.com>
2020-12-17 16:41:05 +0800

    fixbug: application error at mock env

--- smart-utility/src/main/java/org/smartframework/cloud/utility/spring/SpringContextUtil.java
+++ smart-utility/src/main/java/org/smartframework/cloud/utility/spring/SpringContextUtil.java
@@ -12,30 +12,28 @@ import org.springframework.context.event.ContextRefreshedEvent;
  */
 public class SpringContextUtil implements ApplicationListener<ContextRefreshedEvent> {
 
-	private static ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		if (SpringContextUtil.applicationContext == null) {
-			SpringContextUtil.applicationContext = event.getApplicationContext();
-		}
-	}
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        SpringContextUtil.applicationContext = event.getApplicationContext();
+    }
 
-	public static ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
-	/**
-	 * 获取class对应的bean
-	 *
-	 * @param <T> 类型
-	 * @return bean
-	 */
-	public static <T> T getBean(Class<T> requiredType) {
-		if (applicationContext == null) {
-			return null;
-		}
-		return applicationContext.getBean(requiredType);
-	}
+    /**
+     * 获取class对应的bean
+     *
+     * @param <T> 类型
+     * @return bean
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(requiredType);
+    }
 
 }
