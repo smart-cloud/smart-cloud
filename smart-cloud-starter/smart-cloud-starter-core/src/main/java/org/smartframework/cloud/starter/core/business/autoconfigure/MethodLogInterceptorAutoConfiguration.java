5f4da01e8cb306a1807bf4a671a6ee8c95848df1
luckyQing <1634753825@qq.com>
2021-03-14 13:25:38 +0800

    feature: add method log aspect support

---
+++ smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/autoconfigure/MethodLogInterceptorAutoConfiguration.java
@@ -0,0 +1,43 @@
+package org.smartframework.cloud.starter.core.business.autoconfigure;
+
+import org.smartframework.cloud.starter.core.business.log.MethodLog;
+import org.smartframework.cloud.starter.core.business.log.MethodLogInterceptor;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultBeanFactoryPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * method日志切面配置
+ *
+ * @author collin
+ * @date 2020-03-14
+ */
+@Configuration
+public class MethodLogInterceptorAutoConfiguration {
+
+    @Bean
+    public MethodLogInterceptor methodLogInterceptor() {
+        return new MethodLogInterceptor();
+    }
+
+    @Bean
+    public AspectJExpressionPointcut methodPointcut() {
+        AspectJExpressionPointcut apiPointcut = new AspectJExpressionPointcut();
+        String methodLogName = MethodLog.class.getTypeName();
+        apiPointcut.setExpression(String.format("@annotation(%s)", methodLogName));
+        return apiPointcut;
+    }
+
+    @Bean
+    public Advisor methodLogAdvisor(final MethodLogInterceptor methodLogInterceptor,
+                                 final AspectJExpressionPointcut methodPointcut) {
+        DefaultBeanFactoryPointcutAdvisor apiLogAdvisor = new DefaultBeanFactoryPointcutAdvisor();
+        apiLogAdvisor.setAdvice(methodLogInterceptor);
+        apiLogAdvisor.setPointcut(methodPointcut);
+
+        return apiLogAdvisor;
+    }
+
+}
---
+++ smart-cloud-starter/smart-cloud-starter-core/src/main/java/org/smartframework/cloud/starter/core/business/log/MethodLogInterceptor.java
@@ -0,0 +1,40 @@
+package org.smartframework.cloud.starter.core.business.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.smartframework.cloud.mask.util.MaskUtil;
+import org.smartframework.cloud.starter.core.constants.SymbolConstant;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.lang.reflect.Method;
+
+/**
+ * method日志打印切面
+ *
+ * @author collin
+ * @date 2021-03-13
+ */
+@Slf4j
+public class MethodLogInterceptor implements MethodInterceptor {
+
+    @Nullable
+    @Override
+    public Object invoke(@Nonnull MethodInvocation invocation) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Method method = invocation.getMethod();
+
+        String tag = method.getDeclaringClass().getSimpleName() + SymbolConstant.DOT + method.getName();
+        Object[] agrs = invocation.getArguments();
+        Object result = null;
+        try {
+            result = invocation.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            log.info("method.log=>{}({}ms)-->agrs={}, result={}", tag, (endTime - startTime), MaskUtil.mask(agrs), MaskUtil.mask(result));
+        }
+        return result;
+    }
+
+}
