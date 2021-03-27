1ade6785ca3fa0f7a7a605b08c8c4a98aece3174
luckyQing <1634753825@qq.com>
2021-03-27 15:00:00 +0800

    feature: upgrade xxl-job

--- smart-cloud-starter/smart-cloud-starter-job/src/main/java/org/smartframework/cloud/starter/job/util/XxlJobLogUtil.java
+++ smart-cloud-starter/smart-cloud-starter-job/src/main/java/org/smartframework/cloud/starter/job/util/XxlJobLogUtil.java
@@ -1,13 +1,12 @@
 package org.smartframework.cloud.starter.job.util;
 
-import com.xxl.job.core.log.XxlJobLogger;
-
+import com.xxl.job.core.context.XxlJobHelper;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * xxl-job日志工具类（会在日志系统、xxl-job中同时写入）
- * 
+ *
  * @author liyulin
  * @date 2019-7-11
  */
@@ -15,24 +14,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class XxlJobLogUtil {
 
-	public static void debug(String format, Object... arguments) {
-		log.debug(format, arguments);
-		XxlJobLogger.log(format, arguments);
-	}
+    public static void debug(String format, Object... arguments) {
+        log.debug(format, arguments);
+        XxlJobHelper.log(format, arguments);
+    }
 
-	public static void info(String format, Object... arguments) {
-		log.info(format, arguments);
-		XxlJobLogger.log(format, arguments);
-	}
+    public static void info(String format, Object... arguments) {
+        log.info(format, arguments);
+        XxlJobHelper.log(format, arguments);
+    }
 
-	public static void warning(String format, Object... arguments) {
-		log.warn(format, arguments);
-		XxlJobLogger.log(format, arguments);
-	}
+    public static void warning(String format, Object... arguments) {
+        log.warn(format, arguments);
+        XxlJobHelper.log(format, arguments);
+    }
 
-	public static void error(String msg, Throwable e) {
-		log.error(msg, e);
-		XxlJobLogger.log(e);
-	}
+    public static void error(String msg, Throwable e) {
+        log.error(msg, e);
+        XxlJobHelper.log(e);
+    }
 
 }
