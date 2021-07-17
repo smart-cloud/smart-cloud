ab1163412131d2ed509fd4f9f537f95d44646b13
luckyQing <1634753825@qq.com>
2021-07-17 12:15:57 +0800

    添加http header参数

---
+++ smart-common-web/src/main/java/org/smartframework/cloud/common/web/constants/SmartHttpHeaders.java
@@ -0,0 +1,24 @@
+package org.smartframework.cloud.common.web.constants;
+
+/**
+ * smart cloud http header参数常量
+ *
+ * @author collin
+ * @date 2021-07-17
+ */
+public interface SmartHttpHeaders {
+
+    /**
+     * http header timestamp
+     */
+    String TIMESTAMP = "smart-timestamp";
+    /**
+     * http header nonce
+     */
+    String NONCE = "smart-nonce";
+    /**
+     * http header token
+     */
+    String TOKEN = "smart-token";
+
+}
