ec659f64249a5f8ed96e43e7600a0a1277f4b160
luckyQing <1634753825@qq.com>
2021-08-24 21:01:00 +0800

    fix test case

---
+++ smart-cloud-starter/smart-cloud-starter-mybatis-plus/src/test/java/org/smartframework/cloud/starter/mybatis/plus/test/prepare/dynamicdatasource/DynamicDatasourceApp.java
@@ -0,0 +1,15 @@
+package org.smartframework.cloud.starter.mybatis.plus.test.prepare.dynamicdatasource;
+
+import org.smartframework.cloud.starter.core.support.annotation.SmartBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SmartBootApplication
+@EnableTransactionManagement
+public class DynamicDatasourceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApp.class, args);
+    }
+
+}
