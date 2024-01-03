951456a6171b7459bb4e6a102e67f42f2d979667
luckyQing <1634753825@qq.com>
2024-01-03 17:23:14 +0800

    feat: 添加TelnetUtil工具类

---
+++ smart-utility/src/main/java/io/github/smart/cloud/utility/TelnetUtil.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2019 collin (1634753825@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.smart.cloud.utility;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * telnet工具类
+ *
+ * @author collin
+ * @date 2023-01-03
+ */
+public class TelnetUtil {
+
+    private TelnetUtil() {
+    }
+
+    /**
+     * 测试telnet 机器端口的连通性
+     *
+     * @param hostname
+     * @param port
+     * @param timeout  单位毫秒
+     * @return
+     */
+    public static boolean telnet(String hostname, int port, int timeout) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(hostname, port), timeout);
+            return socket.isConnected();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
---
+++ smart-utility/src/test/java/io/github/smart/cloud/utility/test/integration/TelnetUtilTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2019 collin (1634753825@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.smart.cloud.utility.test.integration;
+
+import io.github.smart.cloud.utility.TelnetUtil;
+import io.github.smart.cloud.utility.test.integration.prepare.TestApplication;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class TelnetUtilTest {
+
+
+    @Test
+    void testTelnet() throws IOException {
+        Assertions.assertThat(TelnetUtil.telnet("localhost", 8080, 3000)).isTrue();
+        Assertions.assertThat(TelnetUtil.telnet("localhost", 8081, 500)).isFalse();
+    }
+
+}
