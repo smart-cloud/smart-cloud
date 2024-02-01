87347f7ce34c8386a9b1626f7dfbecae72249340
luckyQing <1634753825@qq.com>
2024-02-01 16:20:00 +0800

    fixbug: rename OfflineCheckComponent bean name

--- smart-cloud-starter/smart-cloud-starter-monitor/src/main/java/io/github/smart/cloud/starter/monitor/autoconfigure/MonitorAutoConfiguration.java
+++ smart-cloud-starter/smart-cloud-starter-monitor/src/main/java/io/github/smart/cloud/starter/monitor/autoconfigure/MonitorAutoConfiguration.java
@@ -15,7 +15,6 @@
  */
 package io.github.smart.cloud.starter.monitor.autoconfigure;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
 import io.github.smart.cloud.starter.monitor.AppChangeNotifier;
 import io.github.smart.cloud.starter.monitor.component.GitLabComponent;
@@ -39,7 +38,6 @@ import org.springframework.context.annotation.Configuration;
  * @date 2024-01-16
  */
 @Configuration
-@EnableAdminServer
 public class MonitorAutoConfiguration {
 
     @Bean
@@ -69,7 +67,7 @@ public class MonitorAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public OfflineCheckComponent reminderComponent(final InstanceRepository instanceRepository, final RobotComponent robotComponent, final MonitorProperties monitorProperties,
+    public OfflineCheckComponent offlineCheckComponent(final InstanceRepository instanceRepository, final RobotComponent robotComponent, final MonitorProperties monitorProperties,
                                                    final ReminderComponent reminderComponent) {
         return new OfflineCheckComponent(instanceRepository, robotComponent, monitorProperties, reminderComponent);
     }
