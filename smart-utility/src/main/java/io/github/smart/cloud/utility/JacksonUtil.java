8f2529443ce9536125fed039aacd122cf172cea2
luckyQing <1634753825@qq.com>
2024-02-15 10:44:17 +0800

    refactor: 优化jackson工具类

--- smart-utility/src/main/java/io/github/smart/cloud/utility/JacksonUtil.java
+++ smart-utility/src/main/java/io/github/smart/cloud/utility/JacksonUtil.java
@@ -19,8 +19,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,13 +34,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class JacksonUtil {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
-    static {
-        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        OBJECT_MAPPER.registerModule(new JavaTimeModule());
-    }
+    private static final JsonMapper JSON_MAPPER = JsonMapper.builder()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+            .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true)
+            .addModule(new JavaTimeModule()).build();
 
     private JacksonUtil() {
     }
@@ -53,7 +52,7 @@ public final class JacksonUtil {
     public static String toJson(Object value) {
         String result = null;
         try {
-            result = OBJECT_MAPPER.writeValueAsString(value);
+            result = JSON_MAPPER.writeValueAsString(value);
         } catch (JsonProcessingException e) {
             log.error("write.String.error", e);
         }
@@ -69,7 +68,7 @@ public final class JacksonUtil {
     public static byte[] toBytes(Object value) {
         byte[] bytes = null;
         try {
-            bytes = OBJECT_MAPPER.writeValueAsBytes(value);
+            bytes = JSON_MAPPER.writeValueAsBytes(value);
         } catch (JsonProcessingException e) {
             log.error("write.byte[].error", e);
         }
@@ -86,7 +85,7 @@ public final class JacksonUtil {
     public static <T> T parseObject(String content, Class<T> valueType) {
         T t = null;
         try {
-            t = OBJECT_MAPPER.readValue(content, valueType);
+            t = JSON_MAPPER.readValue(content, valueType);
         } catch (JsonProcessingException e) {
             log.error("parse object error", e);
         }
@@ -104,7 +103,7 @@ public final class JacksonUtil {
     public static <T> T parseObject(String content, TypeReference<T> valueTypeRef) {
         T t = null;
         try {
-            t = OBJECT_MAPPER.readValue(content, valueTypeRef);
+            t = JSON_MAPPER.readValue(content, valueTypeRef);
         } catch (JsonProcessingException e) {
             log.error("parse object error", e);
         }
@@ -115,7 +114,7 @@ public final class JacksonUtil {
     public static JsonNode parse(String content) {
         JsonNode t = null;
         try {
-            t = OBJECT_MAPPER.readTree(content);
+            t = JSON_MAPPER.readTree(content);
         } catch (JsonProcessingException e) {
             log.error("parse object error", e);
         }
